35d3ce523c67587a6b4b0dcc53979633b9278423
Anuraag Muktevi <anuraagmuktevi@Anuraags-MBP.lan>
2023-01-23 10:37:27 -0600

    Implementation of basic atomic design pattern for frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import { Container } from 'reactstrap';
 import './App.css';
-import BlogComponent from './components/BlogComponent';
+import BlogComponent from './components/Blog/BlogComponent';
+import HeaderComponent from './components/Header/HeaderComponent';
 
 function App() {
 
   return (
     <div className="App">
-      <BlogComponent></BlogComponent>
+      <HeaderComponent></HeaderComponent>
+      <section class="section">
+        <BlogComponent></BlogComponent>
+      </section>
     </div>
   );
 }
