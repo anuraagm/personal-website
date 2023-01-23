917de0fa0ea4418a17b7e9b38b7e0737ae0883e1
Anuraag Muktevi <anuraagmuktevi@Anuraags-MBP.lan>
2023-01-22 21:41:41 -0600

    Directory structure cleanup for front-end.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-import axios from "axios";
-
+import BlogComponent from './components/BlogComponent';
 
 function App() {
 
-  const [blog, setBlog] = useState();
-  const [display, setDisplay] = useState(false);
-
-  useEffect(() => {
-    renderItems()
-  },[])
-
-  const renderItems = async() => {
-    const items = await axios.get("/api/blogs/");
-    const posts = items.data;
-    setBlog(posts);
-  };
-
-  const posts = blog?.map((post, i) => (
-    <li key={i} className="list-group-item">
-        <div><span>{post.title}</span></div>
-        <div><span>{post.content}</span></div>
-    </li>
-  ));
-
   return (
     <div className="App">
-      <header className="App-header">
-        <ul className="list-group list-group-flush border-top-0">
-          {posts}
-        </ul>
-      </header>
+      <BlogComponent></BlogComponent>
     </div>
   );
 }
