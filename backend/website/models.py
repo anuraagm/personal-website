904dd442f71c3107ba0072286c81affecdfb29bd
Anuraag Muktevi <anuraagmuktevi@Anuraags-MBP.lan>
2023-01-22 20:16:38 -0600

    Integration of basic front-end and back-end components. Tested with blog posts.

--- backend/website/models.py
+++ backend/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+
+    def _str_(self):
+        return self.title
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BlogSerializer
+from .models import Blog
 
 # Create your views here.
+
+
+class BlogView(viewsets.ModelViewSet):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
