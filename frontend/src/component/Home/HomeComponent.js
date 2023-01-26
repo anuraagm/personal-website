dd3b19d430ab9111ed50eedee798c5fe71ef0e98
Anuraag Muktevi <anuraagmuktevi@Anuraags-MacBook-Pro.local>
2023-01-26 17:08:05 -0600

    Home Page axios call done.

--- frontend/src/component/Home/HomeComponent.js
+++ frontend/src/component/Home/HomeComponent.js
@@ -3,9 +3,13 @@ import WorkComponent from "../Work/WorkComponent";
 
 function HomeComponent () {
     return (
-        <div className="Home mx-auto p-5 grid md:grid-cols-2 sm:grid-cols-1 gap-4">
-            <IntroComponent className="col-span-2"></IntroComponent>
-            <WorkComponent className="col-span-2"></WorkComponent>
+        <div className="Home mx-auto p-5 grid md:grid-cols-7 sm:grid-cols-1 md:p-12">
+            <div className="md:col-start-1 md:col-end-3">
+                <IntroComponent></IntroComponent>
+            </div>
+            <div className="md:col-start-4 md:col-end-7">
+                <WorkComponent className="col-span-4"></WorkComponent>
+            </div>
         </div>
     );
 }
