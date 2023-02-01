725658ea6d09ba14de8e74041f94b7578abdae24
Anuraag Muktevi <anuraagmuktevi@Anuraags-MacBook-Pro.local>
2023-02-01 13:55:32 -0600

    media paths setting and readme update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Personal Website - Anuraag Muktevi
 I have created this repository for the development of my personal website.
+
+1. Clone
+2. Install python and pip
+3. pip install django
+4. pip install django-summernote
+5. pip3 install django-cors-headers
+6. pip3 install djangorestframework
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,8 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
