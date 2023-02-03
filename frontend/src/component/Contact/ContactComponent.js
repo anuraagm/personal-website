b87e0a82a19e0170c4783b4a97e8b58a9854786b
Anuraag Muktevi <anuraagmuktevi@Anuraags-MacBook-Pro.local>
2023-02-02 20:22:54 -0600

    Temporary fix contact component

--- frontend/src/component/Contact/ContactComponent.js
+++ frontend/src/component/Contact/ContactComponent.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import instagram from './instagram.png';
+import wordpress from './wordpress.png';
 
 const ContactComponent = () => {
   const [name, setName] = useState('');
@@ -14,60 +16,52 @@ const ContactComponent = () => {
 
   return (
     <div className="ContactComponent dark:bg-zinc-800 p-6 rounded-lg shadow-md">
-      <form onSubmit={handleSubmit}>
-        <h5 className="text-2xl font-bold text-gray-300 mb-6">Drop in a message and I'll get in touch with you as soon as possible.</h5>
-        <div className="mb-4">
-          <label
-            className="block text-gray-200 font-medium mb-2"
-            htmlFor="name"
-          >
-            Your Name
-          </label>
-          <input
-            className="w-full border border-zinc-600 text-gray-200 bg-zinc-700 p-2"
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-200 font-medium mb-2"
-            htmlFor="email"
-          >
-            Your Email
-          </label>
-          <input
-            className="w-full border border-zinc-600 text-gray-200 bg-zinc-700 p-2"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-200 font-medium mb-2"
-            htmlFor="message"
-          >
-            Your Message
-          </label>
-          <textarea
-            className="w-full border border-zinc-600 text-gray-200 bg-zinc-700 p-2"
-            id="message"
-            rows="5"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-        <button
-          className="bg-zinc-700 hover:bg-zinc-600 text-white font-medium py-2 px-4"
-          type="submit"
+      <h5 className="text-2xl font-bold text-gray-300 mb-6 md:w-2/3">Drop in a message and I'll get in touch with you as soon as possible.</h5>
+      <div className="mb-8">
+        <label
+          className="block font-bold text-gray-200"
+          htmlFor="name"
         >
-          Submit
-        </button>
-      </form>
+          Name
+        </label>
+        <p className="w-full border text-gray-200 border-zinc-800">
+          Anuraag Muktevi
+        </p>
+      </div>
+      <div className="mb-8">
+        <label
+          className="block font-bold text-gray-200"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <p className="w-full border text-gray-200 border-zinc-800">
+          muktevianuraag@gmail.com
+        </p>
+      </div>
+      <div className="mb-8">
+        <label
+          className="block text-gray-200 font-bold"
+          htmlFor="message"
+        >
+          Phone Number
+        </label>
+        <p className="w-full border border-zinc-800 text-gray-200">
+          +1 469-346-0185  
+        </p>
+      </div>
+      <div className="mb-8">
+        <label
+          className="block text-gray-200 font-bold mb-6"
+          htmlFor="message"
+        >
+          Follow me on
+        </label>
+        <div className="grid grid-cols-2">
+            <a href="https://www.instagram.com/thegaaruverse/"><figure><img style={{marginLeft:10}} src={instagram}/><figcaption className="text-xxs text-gray-100">instagram</figcaption></figure></a>
+            <a href="https://thegaaruverse.wordpress.com/"><figure><img style={{marginLeft:10}} src={wordpress}/><figcaption className="text-xxs text-gray-100">wordpress</figcaption></figure></a>
+        </div>
+      </div>
     </div>
   );
 };
